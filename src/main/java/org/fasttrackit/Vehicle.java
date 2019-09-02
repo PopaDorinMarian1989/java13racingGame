ce79440e54ff5ab6a95aacadbbd2c97bf054c807
popa dorin <dorinmarian89@gmail.com>
2019-09-02 20:04:31 +0300

    studied classes, variables ,primitive,data type and aggregation added accelerate behavior for vehicle

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -10,4 +10,21 @@ public class Vehicle{
     double traveledDistance;
     boolean running;
 
+    public double accelerate (double speed, double durationinHours) {
+        System.out.println(name + " is accelerating with" + speed + " km/h for " + durationinHours + " h.");
+        double distance = speed * durationinHours;
+
+        System.out.println("Distance : " + distance);
+
+        traveledDistance = traveledDistance + distance;
+        // same result as the above statment
+//        traveledDistance += distance;
+        double usedFuel = distance * mileage / 100;
+        System.out.println("Used fuel: + usedfuel");
+        fuelLevel -= usedFuel;
+        System.out.println("Remaining fuel: " + fuelLevel);
+
+
+        return distance;
+    }
 }
