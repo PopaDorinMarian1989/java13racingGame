ab1425e300aa6bef02f011b6adbae97e8da187f3
popa dorin <dorinmarian89@gmail.com>
2019-09-02 19:07:02 +0300

    studied classes, variables ,primitive,data type and aggregation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -18,6 +18,7 @@ public class App
         car.maxSpeed = 180;
         car.fuelType = "Gasoline";
 
+
         Engine engine = new Engine();
         engine.manufacturer = "Renault";
         engine.capacity = 1600;
